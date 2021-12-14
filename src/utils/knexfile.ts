60cf0d88f1103529a65e8db4a6ec2786f0a12dee
Vinicius Finger <vinifinger92@gmail.com>
2021-12-13 21:20:14 -0300

    Configuraçao de pools do Knex

--- src/utils/knexfile.ts
+++ src/utils/knexfile.ts
@@ -1,14 +1,18 @@
 import * as path from 'path';
-import * as dotenv from 'dotenv';
 import { Knex } from 'knex';
+import * as dotenv from 'dotenv';
 dotenv.config();
 const config: Knex.Config = {
-    client: 'mysql',
+    client: 'mysql2',
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_BASE,
+    },
+    pool: {
+        min: 1,
+        max: 20,
     },
     migrations: {
         directory: path.resolve(__dirname, 'src', 'database', 'migrations')
